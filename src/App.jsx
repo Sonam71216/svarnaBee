e697c0695d7bd7683b6c7787ef649e905aa23b78
Sonam Verma <snm71216@gmail.com>
2026-06-01 20:59:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Home from './Home'
+import Logo from './assets/images/footer-logo.png'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='min-h-screen flex flex-col items-center justify-center text-center'>
+            <div className='logo-img'>
+                <img src={Logo} alt="" className='mx-auto'/>
+            </div>
+          <h1 className='text-[60px]  bg-gradient-to-r from-[#afafaf] to-[#afafaf] bg-clip-text text-transparent'>Coming Soon</h1>
+          <Home />
+        </div>
+        
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+
+const Home = () => {
+  const birthday = new Date("2026-06-16T03:43:16");
+
+  const getTimeLeft = () => {
+    const difference = birthday - new Date();
+
+    if (difference <= 0) {
+      return {
+        days: "00",
+        hours: "00",
+        minutes: "00",
+        seconds: "00",
+      };
+    }
+
+    return {
+      days: String(
+        Math.floor(difference / (1000 * 60 * 60 * 24))
+      ).padStart(2, "0"),
+
+      hours: String(
+        Math.floor((difference / (1000 * 60 * 60)) % 24)
+      ).padStart(2, "0"),
+
+      minutes: String(
+        Math.floor((difference / (1000 * 60)) % 60)
+      ).padStart(2, "0"),
+
+      seconds: String(
+        Math.floor((difference / 1000) % 60)
+      ).padStart(2, "0"),
+    };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <section>
+      <div className="flex flex-wrap justify-center gap-4 md:gap-20 py-6  md:py-8 text-center">
+        <div className="text-center">
+          <h2 className="text-[#C8AB6B] text-4xl md:text-8xl font-semibold bg-[#f1f1f121] pt-2 py-3 px-5 rounded-lg">
+            {timeLeft.days}
+          </h2>
+          <p className="text-[#C8AB6B] text-sm md:text-2xl mt-3 ">
+            DAYS
+          </p>
+        </div>
+
+        <div className="text-center">
+          <h2 className="text-[#C8AB6B] text-4xl md:text-8xl font-semibold bg-[#f1f1f121] pt-2 py-3 px-5 rounded-lg">
+            {timeLeft.hours}
+          </h2>
+          <p className="text-[#C8AB6B]  text-sm  md:text-2xl mt-3">
+            HOURS
+          </p>
+        </div>
+
+        <div className="text-center">
+          <h2 className="text-[#C8AB6B] text-4xl md:text-8xl font-semibold bg-[#f1f1f121] pt-2 py-3 px-5 rounded-lg">
+            {timeLeft.minutes}
+          </h2>
+          <p className="text-[#C8AB6B] text-sm md:text-2xl mt-3">
+            MINUTES
+          </p>
+        </div>
+
+        <div className="text-center">
+          <h2 className="text-[#C8AB6B] text-4xl md:text-8xl font-semibold bg-[#f1f1f121] pt-2 py-3 px-5 rounded-lg">
+            {timeLeft.seconds}
+          </h2>
+          <p className="text-[#C8AB6B] text-sm md:text-2xl mt-3">
+            SECONDS
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
